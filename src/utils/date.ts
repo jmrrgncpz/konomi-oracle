dfe0096e0aa9d5080a8fe38b023f6e1823c21093
jmrrgncpz <paz.jesr@gmail.com>
2022-03-20 23:37:39 +0800

    * include json-server and conccurently to app deps * fix date formatting

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,17 @@
+const formatter = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric', month: '2-digit', day: '2-digit',
+  hour: '2-digit', minute: '2-digit', second: '2-digit',
+  hour12: false,
+});
+
 export const formatDateString = (dateString: string) => {
   var b = dateString.split(/\D+/);
   const date = new Date(Date.UTC(Number(b[0]), Number(b[1]) - 1, Number(b[2]), Number(b[3]), Number(b[4]), Number(b[5]), Number(b[6])));
-
-  const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
-  const month = new Intl.DateTimeFormat('en', { month: 'short'}).format(date);
-  const day = new Intl.DateTimeFormat('en', { day: '2-digit'}).format(date);
-  const hour = new Intl.DateTimeFormat('en', { hour: '2-digit', hour12: false }).format(date);
-  const minute = new Intl.DateTimeFormat('en', { minute: '2-digit' }).format(date);
+  const parts = formatter.formatToParts(date);
+  const { day, month, year, hour, minute } = parts.reduce((acc: { [key: string]: string }, curr) => {
+    acc[curr.type] = curr.value;
+    return acc;
+  }, {})
 
   return `${day}/${month}/${year} ${hour}:${minute}`;
 }
